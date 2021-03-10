e33a1e127e3808b7481c2ec81180dbfd446b2eb7
treetzeng <treetzeng@lctech.com.tw>
2021-03-10 21:51:02 +0800

    add ecapay test case

--- test/ecpay.spec.ts
+++ test/ecpay.spec.ts
@@ -1,5 +1,23 @@
-describe('ecpay', () => {
-    it('encrypt method', () => {
-        
+import { Ecpay } from "../src/Ecpay";
+
+const product = {
+    Name: "Test",
+    ID: "A123456789"
+};
+
+const str = '7woM9RorZKAtXJRVccAb0qhHYm+5lnlhBzyfh5EZdNck7PacNsRHgv/Jvp//ajJidqcQcs0UmAgPQVjXQHeziw==';
+
+const ecpay = new Ecpay({
+    key: 'A123456789012345',
+    iv: 'B123456789012345',
+});
+
+describe("ecpay", () => {
+    it("encrypt method should work", () => {
+        expect(ecpay.encrypt(product)).toBe(str);
+    });
+
+    it('decrypt method should work', () => {
+        expect(ecpay.decrypt(str)).toStrictEqual(product);
     });
 });
